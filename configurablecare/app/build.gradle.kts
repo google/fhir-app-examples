4ff23c2a27b0133d0c67025f4f5c75d9c932d10b
Divya Ramnath <divyaramnath@google.com>
2023-07-27 14:23:08 +0530

    Remove unused dependencies

--- configurablecare/app/build.gradle.kts
+++ configurablecare/app/build.gradle.kts
@@ -69,44 +69,7 @@ configurations.all {
 }
 
 object Versions {
-  object Androidx {
-    const val activity = "1.7.2"
-    const val appCompat = "1.6.1"
-    const val constraintLayout = "2.1.4"
-    const val coreKtx = "1.10.1"
-    const val datastorePref = "1.0.0"
-    const val fragmentKtx = "1.6.0"
-    const val lifecycle = "2.6.1"
-    const val navigation = "2.6.0"
-    const val recyclerView = "1.3.0"
-    const val room = "2.5.2"
-    const val sqliteKtx = "2.3.1"
-    const val workRuntimeKtx = "2.8.1"
-  }
-
-  object Cql {
-    const val engine = "2.4.0"
-    const val evaluator = "2.4.0"
-    const val translator = "2.4.0"
-  }
-
-  object Glide {
-    const val glide = "4.14.2"
-  }
-
-  object Kotlin {
-    const val kotlinCoroutinesCore = "1.7.2"
-    const val stdlib = "1.8.20"
-  }
-
-  const val androidFhirCommon = "0.1.0-alpha04"
-  const val androidFhirEngine = "0.1.0-beta03"
-  const val androidFhirKnowledge = "0.1.0-alpha01"
-  const val desugarJdkLibs = "2.0.3"
   const val caffeine = "2.9.1"
-  const val fhirUcum = "1.0.3"
-  const val gson = "2.9.1"
-  const val guava = "28.2-android"
 
   // Hapi FHIR and HL7 Core Components are interlinked.
   // Newer versions of HapiFhir don't work on Android due to the use of Caffeine 3+
@@ -116,45 +79,6 @@ object Versions {
   // Newer versions don't work on Android due to Apache Commons Codec:
   // Wait for this fix: https://github.com/hapifhir/org.hl7.fhir.core/issues/1046
   const val hapiFhirCore = "5.6.36"
-
-  const val http = "4.11.0"
-  // Maximum version that supports Android API Level 24:
-  // https://github.com/FasterXML/jackson-databind/issues/3658
-  const val jackson = "2.13.5"
-  const val jsonToolsPatch = "1.13"
-  const val jsonAssert = "1.5.1"
-  const val material = "1.9.0"
-  const val retrofit = "2.9.0"
-  const val sqlcipher = "4.5.4"
-  const val timber = "5.0.1"
-  const val truth = "1.1.5"
-  const val woodstox = "6.5.1"
-  const val xerces = "2.12.2"
-  const val xmlUnit = "2.9.1"
-
-  // Test dependencies
-  object AndroidxTest {
-    const val benchmarkJUnit = "1.1.1"
-    const val core = "1.5.0"
-    const val archCore = "2.2.0"
-    const val extJunit = "1.1.5"
-    const val rules = "1.5.0"
-    const val runner = "1.5.0"
-    const val fragmentVersion = "1.6.0"
-  }
-
-  const val espresso = "3.5.1"
-  const val jacoco = "0.8.10"
-  const val junit = "4.13.2"
-  const val mockitoKotlin = "3.2.0"
-  const val mockitoInline = "4.0.0"
-  const val robolectric = "4.10.3"
-
-  object Mlkit {
-    const val barcodeScanning = "16.1.1"
-    const val objectDetection = "16.2.3"
-    const val objectDetectionCustom = "16.3.1"
-  }
 }
 
 object HapiFhir {
