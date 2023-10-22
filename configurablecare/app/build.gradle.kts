28a1c4dbe9a26bc8e387dea7378672c876c872d9
Jing Tang <jingtang@google.com>
2023-10-22 18:06:02 +0200

    Update Android FHIR SDK dependencies to the latest versions

--- configurablecare/app/build.gradle.kts
+++ configurablecare/app/build.gradle.kts
@@ -137,8 +137,8 @@ dependencies {
   implementation("com.jakewharton.timber:timber:5.0.1")
   implementation("net.openid:appauth:0.11.1")
   implementation("com.auth0.android:jwtdecode:2.0.1")
-  implementation("com.google.android.fhir:engine:0.1.0-beta03")
+  implementation("com.google.android.fhir:engine:0.1.0-beta05")
   implementation("com.google.android.fhir:data-capture:1.0.0")
-  implementation("com.google.android.fhir:knowledge:0.1.0-alpha01")
-  implementation("com.google.android.fhir:workflow:0.1.0-alpha03")
+  implementation("com.google.android.fhir:knowledge:0.1.0-alpha03")
+  implementation("com.google.android.fhir:workflow:0.1.0-alpha04")
 }
